834e90ae7c22eda201862204357046b5ec604bba
Tejas Sathe <Tejassathe010@gmail.com>
2024-03-31 21:24:51 -0400

    Setup login functionality on the frontend

--- client/src/pages/userAuthForm.page.jsx
+++ client/src/pages/userAuthForm.page.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Link, Navigate } from 'react-router-dom';
 import { Toaster, toast } from 'react-hot-toast';
 import axios from 'axios';
 
@@ -7,14 +7,17 @@ import googleIcon from '../imgs/google.png';
 import InputBox from '../components/input.component';
 import AnimationWrapper from '../common/page-animation';
 import { storeInSession } from '../common/session';
+import { userContext } from '../App';
 
 const UserAuthForm = ({type}) => {
 
+    let { userAuth: { access_token }, setUserAuth } = useContext(userContext);
     const userAuthThroughServer = (serverRoute, formData) => {
         console.log(import.meta.env.VITE_SERVER_DOMAIN);
         axios.post(import.meta.env.VITE_SERVER_DOMAIN + serverRoute, formData)
         .then(({ data }) => {
             storeInSession("user", JSON.stringify(data));
+            setUserAuth(data);
         })
         .catch(({ response }) => {
             toast.error(response.data.error);
@@ -44,6 +47,9 @@ const UserAuthForm = ({type}) => {
         userAuthThroughServer(serverRoute, formData);
     }
   return (
+    access_token ? 
+    <Navigate to="/" />
+    :
     <AnimationWrapper keyValue={type}>
         <section className='h-cover flex items-center justify-center'>
             <Toaster />
