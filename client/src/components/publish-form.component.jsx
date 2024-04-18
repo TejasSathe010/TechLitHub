90c0637fb615c9c536515241f513742563f2521e
Tejas Sathe <Tejassathe010@gmail.com>
2024-04-18 00:29:42 -0400

    Complete final publish form

--- client/src/components/publish-form.component.jsx
+++ client/src/components/publish-form.component.jsx
@@ -7,7 +7,7 @@ import Tag from "./tags.component";
 
 const PublishForm = () => {
   let characterLimit = 200;
-  let tagLimint = 10;
+  let tagLimit = 10;
   let {
     blog,
     blog: { banner, title, tags, des },
@@ -38,12 +38,12 @@ const PublishForm = () => {
     if (e.keyCode == 13 || e.keyCode == 188) {
       e.preventDefault();
       let tag = e.target.value;
-      if (tags.length < tagLimint) {
+      if (tags.length < tagLimit) {
         if (!tags.includes(tag) && tag.length) {
           setBlog({ ...blog, tags: [...tags, tag] })
         }
       } else {
-        toast.error(`You can add max ${tagLimint}`);
+        toast.error(`You can add max ${tagLimit}`);
       }
       e.target.value = "";
     }
@@ -99,10 +99,14 @@ const PublishForm = () => {
             <input type="text" placeholder="Topics" className="sticky input-box bg-white top-0  left-0 pl-4 mb-3 focus:bg-white" onKeyDown={handleKeyDown} />
             {
               tags.map((tag, i) => (
-                 <Tag tag={tag} key={i} />
+                 <Tag tag={tag} tagIndex={i} key={i} />
               ))
             }
           </div>
+          <p className="mt-1 mb-4 text-dark-grey text-sm text-right">
+            {tagLimit - tags.length} Tags left
+          </p>
+          <button className="btn-dark px-8">Publish</button>
         </div>
       </section>
     </AnimationWrapper>
