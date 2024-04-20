6b5ca41e1bd01a33f0eaf82c9cb89685cf64449e
Tejas Sathe <Tejassathe010@gmail.com>
2024-04-19 20:03:13 -0400

    Add latest-blogs functionality

---
+++ client/src/pages/home.page.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import AnimationWrapper from '../common/page-animation';
+import InPageNavigation from '../components/inpage-navigation.component';
+
+const HomePage = () => {
+  return (
+    <AnimationWrapper>
+        <section className='h-cover flex justify-center gap-10'>
+            <div className='w-full'>
+                <InPageNavigation routes={["home", "trending blogs"]} defaultHidden={['trending blogs']}>
+                    <h1>Latest blogs here</h1>
+                    <h1>Trending blogs here</h1>
+
+                </InPageNavigation>
+            </div>
+            <div></div>
+        </section>
+    </AnimationWrapper>
+  )
+}
+
+export default HomePage;
